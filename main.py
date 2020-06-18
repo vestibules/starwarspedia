da5b0090a23ef8efc2a97b17b7ba93ee750cc078
vestibules <clementfoursans@gmail.com>
2020-06-18 10:55:36 +0200

    added some error handling for choices in main menu

--- main.py
+++ main.py
@@ -3,25 +3,35 @@ import characters, planets,starships
 encyclo = True
 
 def mainMenu():
+    choice = ''
     menu = {'1' : 'Planètes', '2' : 'Personnages', '3' : 'Vaisseaux'}
     print('Voici la liste des éléments consultables dans cette encyclopédie :')
     for k,v in menu.items():
         print(f'{k} : {v}')
-    choice = input()
-    if choice == '1':
-        planets.planetInfo()
-    elif choice == '2':
-        characters.swchar()
-    elif choice == '3':
-        starships.swstarship()
+    while choice not in menu.keys():
+        print('Sélectionner la section souhaitée :')
+        choice = input()
+        if choice == '1':
+            planets.planetInfo()
+        elif choice == '2':
+            characters.swchar()
+        elif choice == '3':
+            starships.swstarship()
+        else:
+            print('Sélection inconnue.')
 
 def replay():
-    print('Souhaitez vous faire une autre recherche ? o/n')
-    choice = input()
-    if choice == 'o':
-        return True
-    elif choice == 'n':
-        return False
+    choice = ''
+    while choice.lower() != 'o' or choice != 'n':
+        print('Souhaitez vous faire une autre recherche ? o/n')
+        choice = input()
+        if choice == 'o':
+            return True
+        elif choice == 'n':
+            print("Fermeture de l'encyclopédie ...")
+            return False
+        else:
+            print('Sélection inconnue')
 
 while encyclo:
     mainMenu()
